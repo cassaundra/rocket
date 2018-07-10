c7d024e8ef922ba80ac9f5cc0e33f25ad59b0cd9
Cassaundra Smith <cass@cassaundra.io>
2018-07-10 08:57:24 -0700

    Convert tabs to spaces and use delegation

--- src/main/kotlin/io/cassaundra/rocket/Color.kt
+++ src/main/kotlin/io/cassaundra/rocket/Color.kt
@@ -1,24 +1,24 @@
 package io.cassaundra.rocket
 
 data class Color private constructor(val midiVelocity: Int, val darkerMidiVelocity: Int = midiVelocity, val channel: Int = 0) {
-    fun getSolid(): Color =
-            Color(midiVelocity, darkerMidiVelocity, 0)
+	fun getSolid(): Color =
+			Color(midiVelocity, darkerMidiVelocity, 0)
 
-    fun getFlashing(): Color =
-            Color(midiVelocity, darkerMidiVelocity, 1)
+	fun getFlashing(): Color =
+			Color(midiVelocity, darkerMidiVelocity, 1)
 
-    fun getPulsing(): Color =
-            Color(midiVelocity, darkerMidiVelocity, 2)
+	fun getPulsing(): Color =
+			Color(midiVelocity, darkerMidiVelocity, 2)
 
-    companion object {
-        @JvmField val OFF = Color(0)
-        @JvmField val WHITE = Color(3, 1)
-        @JvmField val PINK = Color(57, 59)
-        @JvmField val RED = Color(5, 7)
-        @JvmField val ORANGE = Color(9, 11)
-        @JvmField val YELLOW = Color(13, 15)
-        @JvmField val GREEN = Color(122, 123)
-        @JvmField val TURQOISE = Color(33, 35)
-        @JvmField val BLUE = Color(45, 47)
-    }
+	companion object {
+		@JvmField val OFF = Color(0)
+		@JvmField val WHITE = Color(3, 1)
+		@JvmField val PINK = Color(57, 59)
+		@JvmField val RED = Color(5, 7)
+		@JvmField val ORANGE = Color(9, 11)
+		@JvmField val YELLOW = Color(13, 15)
+		@JvmField val GREEN = Color(122, 123)
+		@JvmField val TURQOISE = Color(33, 35)
+		@JvmField val BLUE = Color(45, 47)
+	}
 }
--- src/main/kotlin/io/cassaundra/rocket/Midi/MidiDeviceConfiguration.kt
+++ src/main/kotlin/io/cassaundra/rocket/Midi/MidiDeviceConfiguration.kt
@@ -6,44 +6,44 @@ import javax.sound.midi.MidiUnavailableException
 
 class MidiDeviceConfiguration(val inputDevice: MidiDevice?, val outputDevice: MidiDevice?) {
 
-    companion object {
-        const val DEVICE_SIGNATURE = "Launchpad MK2"
+	companion object {
+		const val DEVICE_SIGNATURE = "Launchpad MK2"
 
-        @Throws(MidiUnavailableException::class)
-        fun autodetect(): MidiDeviceConfiguration {
-            val inputDevice = autodetectInputDevice()
-            val outputDevice = autodetectOutputDevice()
-            return MidiDeviceConfiguration(inputDevice, outputDevice)
-        }
+		@Throws(MidiUnavailableException::class)
+		fun autodetect(): MidiDeviceConfiguration {
+			val inputDevice = autodetectInputDevice()
+			val outputDevice = autodetectOutputDevice()
+			return MidiDeviceConfiguration(inputDevice, outputDevice)
+		}
 
-        @Throws(MidiUnavailableException::class)
-        private fun autodetectOutputDevice(): MidiDevice? {
-            val midiDeviceInfo = MidiSystem.getMidiDeviceInfo()
-            for (info in midiDeviceInfo) {
-                if (info.description.contains(DEVICE_SIGNATURE) || info.name.contains(DEVICE_SIGNATURE)) {
-                    val device = MidiSystem.getMidiDevice(info)
-                    if (device.maxReceivers == -1) {
-                        return device
-                    }
-                }
-            }
-            return null
-        }
+		@Throws(MidiUnavailableException::class)
+		private fun autodetectOutputDevice(): MidiDevice? {
+			val midiDeviceInfo = MidiSystem.getMidiDeviceInfo()
+			for (info in midiDeviceInfo) {
+				if (info.description.contains(DEVICE_SIGNATURE) || info.name.contains(DEVICE_SIGNATURE)) {
+					val device = MidiSystem.getMidiDevice(info)
+					if (device.maxReceivers == -1) {
+						return device
+					}
+				}
+			}
+			return null
+		}
 
-        @Throws(MidiUnavailableException::class)
-        private fun autodetectInputDevice(): MidiDevice? {
-            val midiDeviceInfo = MidiSystem.getMidiDeviceInfo()
-            for (info in midiDeviceInfo) {
-                if (info.description.contains(DEVICE_SIGNATURE)) {
-                    val device = MidiSystem.getMidiDevice(info)
-                    if (device.maxTransmitters == -1) {
-                        return device
-                    }
-                    device.close()
-                }
-            }
-            return null
-        }
-    }
+		@Throws(MidiUnavailableException::class)
+		private fun autodetectInputDevice(): MidiDevice? {
+			val midiDeviceInfo = MidiSystem.getMidiDeviceInfo()
+			for (info in midiDeviceInfo) {
+				if (info.description.contains(DEVICE_SIGNATURE)) {
+					val device = MidiSystem.getMidiDevice(info)
+					if (device.maxTransmitters == -1) {
+						return device
+					}
+					device.close()
+				}
+			}
+			return null
+		}
+	}
 
 }
