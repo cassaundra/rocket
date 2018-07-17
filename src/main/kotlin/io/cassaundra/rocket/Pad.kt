191a2fe52036cf6a085b9f74fd26ccad137a42ab
Cassaundra Smith <cassaundra@actuallyeverything.com>
2018-07-16 22:03:06 -0700

    Add outline pad util
    and replace lists with sets

--- src/main/kotlin/io/cassaundra/rocket/Pad.kt
+++ src/main/kotlin/io/cassaundra/rocket/Pad.kt
@@ -9,13 +9,13 @@ class Pad(x: Int, y: Int) {
 	val y: Int = if(y in 0..7) y else throw IllegalArgumentException("y not in 0..7")
 
 	companion object Util {
-		@JvmStatic fun rect(p1: Pad, p2: Pad) : List<Pad> {
+		@JvmStatic fun rect(p1: Pad, p2: Pad) : Set<Pad> {
 			val x1: Int = Math.min(p1.x, p2.x)
 			val x2: Int = Math.max(p1.x, p2.x)
 			val y1: Int = Math.min(p1.y, p2.y)
 			val y2: Int = Math.max(p1.y, p2.y)
 
-			val pads: ArrayList<Pad> = ArrayList()
+			val pads: MutableSet<Pad> = mutableSetOf()
 
 			for(x in x1..x2) {
 				for(y in y1..y2) {
@@ -26,15 +26,39 @@ class Pad(x: Int, y: Int) {
 			return pads
 		}
 
-		@JvmStatic fun line(p1: Pad, p2: Pad) : List<Pad> {
+		@JvmStatic fun rectOutline(p1: Pad, p2: Pad) : Set<Pad> {
+			val pads: MutableSet<Pad> = mutableSetOf()
+
+			// add horizontal
+			val minX = min(p1.x, p2.x)
+			val maxX = max(p1.x, p2.x)
+			for(x in minX..maxX) {
+				pads.add(Pad(x, p1.y))
+				pads.add(Pad(x, p2.y))
+			}
+
+			// add vertical
+			val minY = min(p1.y, p2.y)
+			val maxY = max(p1.y, p2.y)
+			for(y in minY..maxY) {
+				pads.add(Pad(p1.x, y))
+				pads.add(Pad(p2.x, y))
+			}
+
+			println(pads.size)
+
+			return pads
+		}
+
+		@JvmStatic fun line(p1: Pad, p2: Pad) : Set<Pad> {
 			val xDiff = p2.x - p1.x
 			val yDiff = p2.y - p1.y
 
 			val iterations = max(abs(xDiff), abs(yDiff))
 
-			val pads: ArrayList<Pad> = ArrayList()
+			val pads: MutableSet<Pad> = mutableSetOf()
 
-			for(i in (0..iterations)) {
+			for(i in 0..iterations) {
 				val t = i * (1.0 / iterations)
 
 				val x = p1.x + t * xDiff + 0.5
@@ -46,8 +70,8 @@ class Pad(x: Int, y: Int) {
 			return pads
 		}
 
-        @JvmStatic fun all() : List<Pad> {
-			val pads = mutableListOf<Pad>()
+        @JvmStatic fun all() : Set<Pad> {
+			val pads = mutableSetOf<Pad>()
 
 			for(y in (0..7)) {
 				for(x in (0..7)) {
