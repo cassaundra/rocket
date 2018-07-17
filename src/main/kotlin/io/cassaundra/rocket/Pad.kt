86a7a674af5f607fc15baf6c60003faf2aaaeb90
Cassaundra Smith <cass@cassaundra.io>
2018-07-16 21:43:07 -0700

    add getPadsInLine util

--- src/main/kotlin/io/cassaundra/rocket/Pad.kt
+++ src/main/kotlin/io/cassaundra/rocket/Pad.kt
@@ -1,5 +1,9 @@
 package io.cassaundra.rocket
 
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
 class Pad(x: Int, y: Int) {
 	val x: Int = if(x in 0..7) x else throw IllegalArgumentException("x not in 0..7")
 	val y: Int = if(y in 0..7) y else throw IllegalArgumentException("y not in 0..7")
@@ -17,6 +21,26 @@ class Pad(x: Int, y: Int) {
 				for(y in y1..y2) {
 					pads.add(Pad(x, y))
 				}
+			}
+
+			return pads
+		}
+
+		@JvmStatic fun getPadsInLine(p1: Pad, p2: Pad) : List<Pad> {
+			val xDiff = p2.x - p1.x
+			val yDiff = p2.y - p1.y
+
+			val iterations = max(abs(xDiff), abs(yDiff))
+
+			val pads: ArrayList<Pad> = ArrayList()
+
+			for(i in (0..iterations)) {
+				val t = i * (1.0 / iterations)
+
+				val x = p1.x + t * xDiff + 0.5
+				val y = p1.y + t * yDiff + 0.5
+
+				pads.add(Pad(x.toInt(), y.toInt()))
 			}
 
 			return pads
