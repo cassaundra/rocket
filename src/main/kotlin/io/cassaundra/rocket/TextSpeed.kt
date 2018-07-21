46e70ef8422c428a5f0cf8219ef761dfe4653b76
Cassaundra Smith <cass@cassaundra.io>
2018-07-21 13:34:20 -0700

    Add Java documentation to TextSpeed and use consts

--- src/main/kotlin/io/cassaundra/rocket/TextSpeed.kt
+++ src/main/kotlin/io/cassaundra/rocket/TextSpeed.kt
@@ -3,21 +3,30 @@ package io.cassaundra.rocket
 /**
  * Can be used in [Launchpad.displayText] to change the text scrolling speed dynamically.
  *
- * Example Kotlin usage:
+ * **Example Kotlin usage:**
  * ```
  * Rocket.launchpad.displayText(
- *     "${TextSpeed.SPEED_7} Fast text and ${TextSpeed.SPEED_2} slow text",
+ *     "${TextSpeed.SPEED_7}Fast text and ${TextSpeed.SPEED_2}slow text",
  *     Color.WHITE,
  *     Runnable { println("Done") }
  * )
  * ```
+ *
+ * **Example Java usage:**
+ * ```
+ * Rocket.INSTANCE.getLaunchpad().displayText(
+ *     TextSpeed.SPEED_7 + "Fast text and " + TextSpeed.SPEED_2 + "slow text",
+ *     Color.WHITE,
+ *     () -> System.out.println("Done")
+ * );
+ * ```
  */
 object TextSpeed {
-	val SPEED_1 = 1.toChar()
-	val SPEED_2 = 2.toChar()
-	val SPEED_3 = 3.toChar()
-	val SPEED_4 = 4.toChar()
-	val SPEED_5 = 5.toChar()
-	val SPEED_6 = 6.toChar()
-	val SPEED_7 = 7.toChar()
+	const val SPEED_1 = 1.toChar()
+	const val SPEED_2 = 2.toChar()
+	const val SPEED_3 = 3.toChar()
+	const val SPEED_4 = 4.toChar()
+	const val SPEED_5 = 5.toChar()
+	const val SPEED_6 = 6.toChar()
+	const val SPEED_7 = 7.toChar()
 }
