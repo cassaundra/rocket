795815650eb9d03977068ceaba39b74cc9743289
Cassaundra Smith <cass@cassaundra.io>
2018-07-21 22:10:47 -0700

    Make HSV values safe
    hue will loop outside 0 and 1 (using math mod)
    saturation and value are clamped between 0 and 1

--- src/main/kotlin/io/cassaundra/rocket/Color.kt
+++ src/main/kotlin/io/cassaundra/rocket/Color.kt
@@ -1,5 +1,9 @@
 package io.cassaundra.rocket
 
+import java.lang.Math.floorMod
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * A color to be used on the Launchpad. Each RGB value should be between 0 and 63 inclusive.
  */
@@ -14,11 +18,22 @@ data class Color(val red: Int, val green: Int, val blue: Int) {
 		@JvmField val BLUE = Color(0, 0, 63)
 
 		/**
-		 * Converts an HSV color ([h], [s], [v]) to a [Color] to be used on the Launchpad.
+		 * Converts an HSV color ([hue], [saturation], [value]) to a [Color] to be used on the Launchpad.
+		 *
+		 * [hue] will loop when outside the range (negative or positive) of 0 and 1.
+		 * [saturation] and [value] will clamp between 0 and 1.
 		 *
 		 * Each HSV value should be in the 0..1 float range.
 		 */
-		@JvmStatic fun fromHSV(h: Float, s: Float, v: Float): Color {
+		@JvmStatic fun fromHSV(hue: Float, saturation: Float, value: Float): Color {
+			var h = hue % 1f
+			if (h < 0) {
+				h += 1
+			}
+
+			val s = max(0f, min(1f, saturation))
+			val v = max(0f, min(1f, value))
+
 			var r = 0f
 			var g = 0f
 			var b = 0f
