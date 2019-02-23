ae4a8f45ed377764030d3c72397a6b07001c4136
Cassaundra <cass@cassaundra.io>
2019-02-23 10:17:13 -0800

    Add documentation

--- src/main/kotlin/io/cassaundra/rocket/LaunchpadClient.kt
+++ src/main/kotlin/io/cassaundra/rocket/LaunchpadClient.kt
@@ -1,14 +1,43 @@
 package io.cassaundra.rocket
 
 /**
- * Used internally as a generic way of communicating with a MIDI Launchpad.
+ * Used internally as a generic way of communicating with a Launchpad.
+ *
+ * @see[Rocket.client]
  */
 interface LaunchpadClient {
+	/**
+	 * Set this client's [LaunchpadListener]
+	 */
 	fun setListener(listener: LaunchpadListener)
+
+	/**
+	 * Send a pad color change message to the device.
+	 */
 	fun sendPadColor(pad: Pad, color: Color)
+
+	/**
+	 * Send a button color change message to the device.
+	 */
 	fun sendButtonColor(button: Button, color: Color)
+
+	/**
+	 * Send a clear all message to the Launchpad.
+	 */
 	fun clear()
+
+	/**
+	 * Send a pad color change message to the device for all pads. The Launchpad MK2's MIDI design supports this is one message, so it's included here.
+	 */
 	fun sendAllPadColors(color: Color)
+
+	/**
+	 * Display text on the Launchpad. Call [onComplete] when done.
+	 */
 	fun displayText(text: String, color: Int, onComplete: Runnable)
+
+	/**
+	 * Close this connection.
+	 */
 	fun close()
 }
--- src/main/kotlin/io/cassaundra/rocket/LaunchpadScanner.kt
+++ src/main/kotlin/io/cassaundra/rocket/LaunchpadScanner.kt
@@ -1,6 +1,13 @@
 package io.cassaundra.rocket
 
 interface LaunchpadScanner {
+	/**
+	 * Begin scanning for the [LaunchpadClient].
+	 */
 	fun beginScan(onSuccess: Runnable)
+
+	/**
+	 * Stop scanning. Should block until stopped.
+	 */
 	fun stopScan()
 }
